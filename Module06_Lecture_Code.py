4ee8061576bab58cf71363c7f5b3c3c4df868529
Ian Burres <burres@Ians-MBP-2.home>
2024-02-29 10:31:12 -0600

    Added code to Module 6

--- Module06_Lecture_Code.py
+++ Module06_Lecture_Code.py
@@ -2,6 +2,23 @@
 
 Module 06 Lecture Code
 
+r = read
+w = write
+a = append
+r+ = read and write
+w+ = write and read
+a+ = append and read
+rb = read binary
+wb = write binary
+ab = append binary
+rb+ = read and write binary
+wb+ = write and read binary
+ab+ = append and read binary
+
+open() function = open a file.  You can use this to create a file with the "w" mode. 
+close() function = close a file
+with open() as file: = Can be used to open a file and automatically close it when the block is done.  This is a good way to handle files. However, you cannot create a new file with the with statement.  The file must already exist!!!!
+
 '''
 import random
 
@@ -85,11 +102,75 @@ def reading_int_from_files():
 
 def skipping_line_space():
     
-    with open(r'/users/burres/desktop/nurseryRhyme.txt', 'r') as file:
+    # the with statement automatically closes the file when the block is done.  Also, you cannot create a new file with the with statement.  The file must already exist!!!!
+    with open(r'/users/burres/desktop/nurseryRhyme.txt', 'r') as file:    
         for line in file:
             if not line.isspace():
                 print(line, end='')
                 
+
+# passing a file to another function
+def pass_file():
+    file = open(r'/users/burres/desktop/nurseryRhyme.txt', 'r')
+    read_file(file)
+    file.close()
+
+# receiving a file name as a parameter
+def read_file(file_name):
+    with open(file_name, 'r') as file:
+        for line in file:
+            if not line.isspace():
+                print(line, end='')
+                
+
+def read_and_write():
+    file = open(r'/users/burres/desktop/nurseryRhyme.txt', 'r+')
+    for line in file:
+        print(line, end='')
+    file.write("\n\nThis is a new line")
+    file.close() # Remember to close the file when you are done with it.
+    
+# dealing with exceptions.  We wrap the code in a try block and then catch the error in the except block.  This is a good way to handle files.
+def read_file_with_exceptions():
+    try:
+        file = open(r'/users/burres/desktop/nurseryRhyme.txt', 'r')
+        for line in file:
+            print(line, end='')
+    except IOError:
+        print("File not found")
+        
+def loop_with_exceptions():
+    while True:
+        try:
+            user_input = int(input("Enter a number: "))
+            print(f"The number you entered is {user_input}")
+            break
+        except ValueError: # you can have more than one except block
+            print("You did not enter a number.  Try again.")
+            
+def using_finaly():
+    try:
+        file = open(r'/users/burres/desktop/nurseryRhyme.txt', 'r')
+        for line in file:
+            print(line, end='')
+    except IOError:
+        print("File not found")
+    finally: # finally block will always execute
+        file.close()
+        
+def users_line():
+    statement = input("Enter a statement: ")
+    write_line(statement)
+        
+# passing parameters to a function that reads a file
+def write_line(sentence):
+    
+    file = open(r'/users/burres/desktop/user_file.txt', 'w+')
+    file.write(sentence)
+    file.close()
+    #with open(r'users/burres/desktop/user_file.txt', 'w') as file:
+    #    file.write(sentence) 
+    
                 
 def main():
     #read_files_by_line()
@@ -98,8 +179,15 @@ def main():
     #another()
     #append_to_file()
     #writing_integers_to_files()
-    reading_int_from_files()
+    #reading_int_from_files()
     #skipping_line_space()
+    #pass_file()
+    #read_and_write()
+    #read_file_with_exceptions()
+    #loop_with_exceptions()
+    #using_finaly()
+    #users_line()
+    pass
 
 if __name__ == "__main__":
     main()
